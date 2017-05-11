69f6300a39284b516eee570297d4d25470096d46
Dave <david.noel.0306@gmail.com>
2017-05-11 17:47:53 -0400

    Add files via upload

---
+++ server/services/transaction.service.js
@@ -0,0 +1,42 @@
+﻿var config = require('config.json');
+var _ = require('lodash');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var Q = require('q');
+var mongo = require('mongoskin');
+var db = mongo.db(config.connectionString, { native_parser: true });
+db.bind('transactions')
+
+var service = {};
+service.savetrx = savetrx;
+service.getAllTransactions = getAllTransactions;
+
+module.exports = service;
+
+function getAllTransactions() {
+    debugger;
+    var deferred = Q.defer();
+
+    db.transactions.find().toArray(function (err, transactions) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        // return transactions
+        deferred.resolve(transactions);
+    });
+
+    return deferred.promise;
+}
+
+function savetrx(transaction) {
+    debugger;
+    var deferred = Q.defer();
+    db.transactions.insert(
+        transaction,
+        function (err, doc) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            deferred.resolve();
+        });
+
+    return deferred.promise;
+}
